8ff1f350043f8d4130e1a4514b70771f78497c2d
dbsxo <dbsxo@DESKTOP-SOB0598>
2023-11-08 10:14:13 +0900

    테스트 1

---
+++ bubble/src/bubble/test/ex00/LombokTest.java
@@ -0,0 +1,14 @@
+package bubble.test.ex00;
+
+class Dog {
+	private String name;
+}
+
+public class LombokTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
